666d9e15536330ba35d31ba0fa0ba8590a93d86a
DIYA KRISHNA <diyakrishnaah@gmail.com>
2025-10-12 10:00:29 +0530

    Update AppLauncher.java

--- src/com/rebook/main/AppLauncher.java
+++ src/com/rebook/main/AppLauncher.java
@@ -1,9 +1,9 @@
 package com.rebook.main;
-import com.rebook.view.*;
-
+import  com.rebook.view.LoginPanel;
+import com.rebook.view.Dashboard;
 public class AppLauncher {
     public static void main(String[] args) {
+         SwingUtilities.invokeLater(() -> new Dashboard().setVisible(true));
          new LoginPanel();
-
     }
 }
